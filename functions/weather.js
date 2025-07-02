92fe89efba6db59370ac4292030d7b137e6ff51e
Noel Paton-og <yushikamuru@gmail.com>
2025-07-02 11:01:47 +0800

    fixed CORS for weather.js

--- functions/weather.js
+++ functions/weather.js
@@ -4,10 +4,12 @@ exports.handler = async (event) => {
   const apiKey = process.env.WEATHER_API_KEY;
   const privateKey = process.env.PRIVATE_API_KEY;
 
+  const origin = event.headers.origin || "*";
   const headers = {
-    "Access-Control-Allow-Origin": "*", // Or specify a domain instead of "*"
+    "Access-Control-Allow-Origin": origin,
     "Access-Control-Allow-Headers": "Content-Type, x-api-key",
     "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    "Vary": "Origin",
   };
 
   // Handle CORS preflight (OPTIONS)
